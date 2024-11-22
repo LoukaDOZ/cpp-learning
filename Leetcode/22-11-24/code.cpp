55ce034c942be2c95784cb866b0805ec5a280a84
doz <dozlouka@gmail.com>
2024-11-22 11:53:57 +0100

    22/11/24

---
+++ Leetcode/22-11-24/code.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        map<string, int> rowCount;
+        int best = 0;
+
+        for(vector<int> row: matrix) {
+            stringstream s;
+            string key;
+
+            if(row[0] == 0) {
+                for(int col: row)
+                    s << col;
+            } else {
+                for(int col: row)
+                    s << ((col + 1) % 2);
+            }
+
+            key = s.str();
+            if(rowCount.find(key) == rowCount.end())
+                rowCount.insert({key, 1});
+            else
+                rowCount[key] += 1;
+            
+            if(rowCount[key] > best)
+                best = rowCount[key];
+        }
+
+        return best;
+    }
+};
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cpp-learning
 
 ### Leetcode
+- [22/11/24 - Flip Columns For Maximum Number of Equal Rows](./Leetcode/22-11-24/)
 - [21/11/24 - Count Unguarded Cells in the Grid](./Leetcode/21-11-24/)
 - [20/11/24 - Take K of Each Character From Left and Right](./Leetcode/20-11-24/)
 - [19/11/24 - Maximum Sum of Distinct Subarrays With Length K](./Leetcode/19-11-24/)
