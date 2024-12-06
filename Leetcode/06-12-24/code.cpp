954557fdd9cee6a8725ca120196320ea084ec3e0
doz <dozlouka@gmail.com>
2024-12-06 19:00:00 +0100

    leetcode 06-12-24

---
+++ Leetcode/06-12-24/code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        int count = 0;
+        unordered_set<int> ban;
+
+        for(int b: banned) {
+            if(b <= n)
+                ban.insert(b);
+        }
+
+        for(int i = 1; i <= n && maxSum - i >= 0; i++) {
+            if(ban.count(i))
+                continue;
+            
+            maxSum -= i;
+            count++;
+        }
+
+        return count;
+    }
+};
